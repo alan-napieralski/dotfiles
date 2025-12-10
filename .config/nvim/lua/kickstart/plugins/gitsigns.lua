61f8c441b384192bc22bee6b7224335eb2cc3aee
AlanNapieralski <alannapieralski@numiko.com>
2025-12-10 17:01:27 +0000

    Enable blame in gitsigns

--- .config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ .config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -14,6 +14,15 @@ return {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      -- ✅ always‑on inline blame for current line
+      current_line_blame = true, -- or false + use <leader>tb to toggle
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 100,
+        ignore_whitespace = false,
+      },
+  current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> • <summary>',
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
